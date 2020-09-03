b9651fee730f691cf5736478efc360c516052e83
Anil <anilgencdogus@gmail.com>
2020-09-03 19:33:10 -0400

    sample application added

--- AppTier/EC2.tf
+++ AppTier/EC2.tf
@@ -8,5 +8,17 @@ provider "aws" {
 resource "aws_instance" "app" {
     ami = "ami-02354e95b39ca8dec"
     instance_type = "t2.micro"
-    
+
+ user_data = << - EOF 
+   #!/bin/bash -ex
+  yum -y install httpd php mysql php-mysql
+  chkconfig httpd on
+  service httpd start
+  if [ ! -f /var/www/html/lab-app.tgz ]; then
+  cd /var/www/html
+  wget https://aws-tc-largeobjects.s3-us-west-2.amazonaws.com/CUR-TF-200-ACACAD/studentdownload/lab-app.tgz
+  tar xvfz lab-app.tgz
+  chown apache:root /var/www/html/rds.conf.php
+  fi
+
 }
